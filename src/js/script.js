fe6d4453172aeea3dd6f640b6445d35291a5cfa9
Lykkell <git@github.com:lykkell/PM.git>
2020-11-03 14:29:51 +0200

    end of part 1

--- src/js/script.js
+++ src/js/script.js
@@ -73,5 +73,14 @@ const personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
+    privat: false
 };
+const a = prompt ("Один из последних просмотренных фильмов?", ""),
+      b = prompt ("На сколько оцените его?", ""),
+      c = prompt ("Один из последних просмотренных фильмов?", ""),
+      d = prompt ("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
