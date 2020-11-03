4ea2303e936f6c5a949d0b06943ecb8920fb77a9
Lykkell <lykkellit@gmail.com>
2020-11-03 19:28:53 +0200

    Task 1 is done

--- src/js/script.js
+++ src/js/script.js
@@ -76,9 +76,9 @@ const personalMovieDB = {
     privat: false
 };
 const a = prompt ("Один из последних просмотренных фильмов?", ""),
-      b = prompt ("На сколько оцените его?", ""),
+      b = +prompt ("На сколько оцените его?", ""),
       c = prompt ("Один из последних просмотренных фильмов?", ""),
-      d = prompt ("На сколько оцените его?", "");
+      d = +prompt ("На сколько оцените его?", "");
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
