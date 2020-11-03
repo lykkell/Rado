9a286dfb58d76aef41b9707e7d4765dde02404a9
Lykkell <git@github.com:lykkell/PM.git>
2020-11-03 13:58:24 +0200

    add personalMovieDB

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,11 @@
 "use strict";
 
 const numberOfFilms = +prompt ("Сколько фильмов Вы уже посмотрели", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
